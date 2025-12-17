00a19ac9c8b4984964c64297d4e81b72b4cec0d3
toast <toast@ruka.org>
2025-12-16 17:32:59 -0800

    remove wait for key; assume it'll be done elsewhere

--- memdisk_uefi.c
+++ memdisk_uefi.c
@@ -472,18 +472,6 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
         }
     }
 
-
-    Status = ST->ConIn->Reset(ST->ConIn, FALSE);
-    if (EFI_ERROR(Status)) {
-        return Status;
-    }
-    
-    /* Now wait until a key becomes available.  This is a simple
-       polling implementation.  You could try and use the WaitForKey
-       event instead if you like */
-    while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &Key)) == EFI_NOT_READY) {
-    
-    }
     return gDownloadStatus;
 }
 
